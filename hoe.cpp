348c7b41bc56175e1d3e2792c5d4d07ba272bf45
NCCHANG <changhoehin2005@gmail.com>
2024-04-16 17:14:37 +0800

    headache

---
+++ hoe.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+int height = 8;
+int width = 33;
+
+class layout {
+    void plusMinus(int x) {
+        if (x%4 == 0) {
+            cout << '+';
+        } else {
+            cout << '-';
+        }
+    }
+
+    void plusMinusNotBorder(int x) {
+        if ( x <= 4 || x >= width - 5) {
+            plusMinus(x);
+        } else {
+            cout << ' ';
+        }
+    }
+
+    void straightLineBorder (int x) {
+        if (x%4 == 0) {
+                cout << '|';
+            } else {
+                cout << ' ';
+            }
+    }
+
+    void straightLineNotBorder (int x) {
+        if ( x==0 || x==4 || x==width - 5 || x==width - 1) {
+            cout << '|';
+        } else {
+            cout << ' ';
+        }
+    }
+    
+    public:
+
+    void printLayout() {
+        for (int x = 0; x < width; x++) // +---+---+---
+        {
+            plusMinus(x);
+        }
+
+
+        for (int y = 0; y < height; y++)
+        {
+            cout << '\n';
+            
+            for (int x = 0; x < width; x++) //|   |   |   |   | 
+            {
+                if (y == 0 || y == height-1) {
+                    straightLineBorder(x);
+                }
+                else {
+                    straightLineNotBorder(x);
+                }
+            }
+
+            cout << '\n';
+
+            for (int x = 0; x < width; x++) // +---+---+---
+            {
+                if (y==0 || y==height-2 || y==height-1)
+                {
+                    plusMinus(x);
+                } else {
+                    plusMinusNotBorder(x);
+                }
+            }
+        }
+        }
+
+};
+int main()
+{
+    layout l;
+    l.printLayout();
+}
