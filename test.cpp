05d66089fbf11d4bac679778706dc0dbe1c2d3b8
Ansonlim2311 <ansonlim2311@gmail.com>
2024-04-17 14:26:20 +0800

    okok

---
+++ test.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+using namespace std;
+
+int height = 8;
+int width = 33;
+char flash = 'f';
+int flashXPosition = 1;
+int flashYPosition = height - 1;
+char superman = 'S';
+int superManXPosition = 3;
+int superManYPosition = height - 1;
+
+int random = 2 + (rand() % 4);
+
+class layout {
+    void plusMinus(int x) { //+---+---+ (depends on the x)
+        if (x%4 == 0) {
+            cout << '+';
+        } else {
+            cout << '-';
+        }
+    }
+
+    void plusMinusNotBorder(int x) { //+---+                +---+ (depends on the x)
+        if ( x <= 4 || x >= width - 5) {
+            plusMinus(x);
+        } else {
+            cout << ' ';
+        }
+    }
+
+    void straightLineBorder (int x, int y) { //|   |   |   |   |   |   |   | (depends on the x)
+        if (x%4 == 0) {
+                cout << '|';
+            } else if (x == flashXPosition && y == flashYPosition) {
+            cout << flash;  
+            } else if (x== superManXPosition && y == superManYPosition)  {
+            cout << superman;
+            } else {
+                cout << ' ';
+            }
+    }
+
+    void straightLineNotBorder (int x, int y) { //|   |                |   | (depends on x)
+        if ( x==0 || x==4 || x==width - 5 || x==width - 1) {
+            cout << '|';
+        } else if (x == flashXPosition && y == flashYPosition) {
+            cout << flash;    
+        } else if (x== superManXPosition && y == superManYPosition)  {
+            cout << superman;
+        } else {
+            cout << ' ';
+        }
+    }
+    
+    public:
+
+    void printLayout() {
+        for (int x = 0; x < width; x++) // +---+---+---
+        {
+            plusMinus(x);
+        }
+
+
+        for (int y = 0; y < height; y++)
+        {
+            cout << '\n';
+            //|   |    | section
+            for (int x = 0; x < width; x++) //|   |   |   |   | 
+            {
+                if (y == 0 || y == height-1) {
+                    straightLineBorder(x, y);
+                }
+                else {
+                    straightLineNotBorder(x, y);
+                }
+            }
+
+            cout << '\n';
+            //plusminus section
+            for (int x = 0; x < width; x++) // +---+---+---
+            {
+                if (y==0 || y==height-2 || y==height-1)
+                {
+                    plusMinus(x);
+                } else {
+                    plusMinusNotBorder(x);
+                }
+            }
+        }
+    }
+
+
+    void flashStep() {
+        srand((unsigned) time(NULL));
+        int random = 2 + (rand() % 4);
+        // cout << random;
+
+        flashYPosition = flashYPosition - random;
+        printLayout();
+    }
+};
+int main()
+{
+    layout l;
+    l.printLayout();
+    cout << '\n' << '\n';
+    l.flashStep();
+
+    return 0;
+}
