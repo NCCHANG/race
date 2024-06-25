7d1d66efc8264a555261df5dd03dc88b8c2cbfe3
Ansonlim2311 <ansonlim2311@gmail.com>
2024-06-25 18:54:11 +0800

    zz

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,8 @@
-
-
-        obstacle.checkObstacle(batmanStep, 2, batmanXPosition, batmanYPosition, batmanLocation, "Batman");
-        obstacle.checkObstacle(supermanStep, 3, supermanXPosition, supermanYPosition, supermanLocation, "Superman");
-        obstacle.checkObstacle(flashStep, 1, flashXPosition, flashYPosition, flashLocation, "Flash");
-        
-        bridge.checkIfAtBridgeNMove(1, flashXPosition,
-            flashYPosition, flashLocation);
-        bridge.checkIfAtBridgeNMove(3, supermanXPosition,
-            supermanYPosition, supermanLocation);
-        bridge.checkIfAtBridgeNMove(2, batmanXPosition,
-            batmanYPosition, batmanLocation);
-
-        l.printLayout({},bridge.bridgeYValues);
-        checkWinner();
-        cout << endl;
-    }
-    cout << endl;
-    system("pause");
+batmanMove();
+        bonus.checkBonus(step, 2, batmanXPosition, batmanYPosition, batmanLocation, "Batman");
+        supermanMove();
+        supermanXPosition -= 1;
+        bonus.checkBonus(step, 3, supermanXPosition, supermanYPosition, supermanLocation, "Superman");
+        flashMove();
+        flashXPosition += 1;
+        bonus.checkBonus(step, 1, flashXPosition, flashYPosition, flashLocation, "Flash");
